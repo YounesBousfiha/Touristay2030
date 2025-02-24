74d828b12e88082b2238372d216883decb139273
Coderyounes <younesanas2021@gmail.com>
2025-02-24 19:58:06 +0000

    (TOUR-13) make a minor update in registration form UI

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -39,6 +39,16 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
+        <!-- Role -->
+        <div class="mt-4">
+           <x-input-label for="roles" />
+            <select id="roles" name="role_id" class="block mt-1 w-full rounded-md border-gray-300">
+                <option value="2">Property Owner</option>
+                <option value="3">Tourist</option>
+            </select>
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
                 {{ __('Already registered?') }}
